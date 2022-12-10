fdad048bcdfe301e792a27a44b00e1a622a54318
krunal <krunal.maru1106@gmail.com>
2022-12-10 16:04:37 +0530

    studentcrud

--- student/api/urls.py
+++ student/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from api import views
 urlpatterns = [
-    path('home/', views.student, name='student'),
+    path('home/', views.student_a, name='student'),
 ]
--- student/api/views.py
+++ student/api/views.py
@@ -3,12 +3,23 @@ from django.http import HttpResponse
 from .models import Student
 from .serializer import studentSerializer
 from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+import io
 # Create your views here.
 
-def student(request):
-    stu = Student.objects.get(roll=102)  #complex datatype
-    serializer = studentSerializer(stu)  #python datatype
-    json_data = JSONRenderer().render(serializer.data) # json data
-    return HttpResponse(json_data, content_type = 'application/json')
-
+def student_a(request):
+    if request.method == 'GET':
+        json_data = request.body
+        stream = io.BytesIO(json_data)
+        pythondata = JSONParser().parse(stream)
+        id = pythondata.get('id',None)
+        if id is not None:
+            stu = Student.objects.get(id=id)
+            serializer = studentSerializer(stu)  #python datatype
+            json_data = JSONRenderer().render(serializer.data) # json data
+            return HttpResponse(json_data, content_type = 'application/json')
+        stu = Student.objects.all()
+        serializer = studentSerializer(stu, many=True)
+        json_data = JSONRenderer().render(serializer.data)
+        return HttpResponse(json_data,content_type = 'application/json')
 
